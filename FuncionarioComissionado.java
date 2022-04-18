8e89ff74d2ddcab726424d743643f951fa65c35f
Nedram <96101327+mardentorres@users.noreply.github.com>
2022-04-18 03:22:04 -0300

    Add files via upload

---
+++ FuncionarioComissionado.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+
+public class FuncionarioComissionado extends Funcionario {
+
+	
+	private int vendas;
+
+	private double comissao;
+
+	
+	FuncionarioComissionado(String primeiro_nome, String segundo_nome, double salario, int vendas){
+
+		
+		super(primeiro_nome,segundo_nome, salario);
+
+		this.vendas=vendas;
+
+	}
+
+	
+	FuncionarioComissionado(){}
+
+	
+	public void comissao()	{
+
+		
+	
+		comissao = getSalario()+(vendas * 0.10);
+
+		this.comissao = comissao;
+		
+
+
+		System.out.println("Salario Comissionado: "+comissao);
+
+		
+	}
+		
+
+	public double getVendas() {
+
+		return vendas;
+
+	}
+
+
+
+	public void setVendas(int vendas) {
+
+		this.vendas = vendas;
+
+	}
+	
+	
+}
